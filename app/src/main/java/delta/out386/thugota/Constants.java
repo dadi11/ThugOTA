f2ec6ee4bf8c63d11ad18635152926bc112e138b
Ritayan Chakraborty <ritayanout@gmail.com>
2016-04-21 16:39:45 +0530

    Dispaly proper messages
    ...if :
    () The delta file is missing
    () The directory is empty
    
    Looked like an app bug earlier.

--- app/src/main/java/delta/out386/thugota/Constants.java
+++ app/src/main/java/delta/out386/thugota/Constants.java
@@ -8,7 +8,8 @@ public class Constants {
     final static String GENERIC_DIALOG = "delta.out386.thugota.GENERIC_DIALOG";
     final static String PROGRESS_DIALOG = "delta.out386.thugota.PROGRESS_DIALOG";
     final static String ACTION_NOT_ROM_DIALOG = "delta.out386.thugota.NOT_ROM_DIALOG";
-    final static String AUTO_UPDATE = "delta.out386.thugota.AUTO_UPDATE_DIALOG";
+    final static String AUTO_UPDATE = "delta.out386.thugota.AUTO_UPDATE";
+    final static String NO_ROMS = "delta.out386.thugota.NO_ROMS";
 
     final static String PROGRESS = "delta.out386.thugota.PROGRESS";
     final static String DIALOG_MESSAGE = "delta.out386.thugota.DIALOG_MESSAGE";
